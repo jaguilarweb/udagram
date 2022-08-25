35ed1d24ab79bd713d1874fedb9984cb1961398f
jaguilarweb <contact@jaguilarweb.com>
2022-08-25 15:31:39 +0000

    fix: Cors and script issues fixed

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -22,8 +22,21 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(bodyParser.json());
 
-  app.use(cors());
-  app.options('*', cors()) // enable pre-flight requests  
+ // app.use(cors());
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS. 
+  app.use(cors({
+    "allowedHeaders": [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Methods'
+    ],
+    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    "preflightContinue": true,
+    "origin": '*',
+  }));
 
   app.use("/api/v0/", IndexRouter);
 
@@ -34,7 +47,8 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Start the Server
   app.listen(port, () => {
-    console.log(`server running ${process.env.URL}`);
+    console.log(`Backend server is listening on port ${port}....`);
+    console.log(`Frontent server running ${process.env.URL}`);
     console.log(`press CTRL+C to stop server`);
   });
 })();
